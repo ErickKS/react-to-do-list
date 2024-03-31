fd59be80bc00cad55a244d50517b250140ed5249
abdelrahman-mh <abdelrahman.mh3@gmail.com>
2024-03-31 18:00:38 +0200

    add task complete sound

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { Howl } from 'howler';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const completeSound = new Howl({
+  src: ['https://res.cdn.office.net/todo/1630603_2.116.1/sounds/complete.wav'],
+  volume: 1,
+  loop: false,
+})
